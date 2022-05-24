1e2dc148aa121b1813d88941396a318fc0f469fb
jhartung <joel.hartung@gmail.com>
2022-05-24 11:45:40 -0500

    Assignment 1.4

--- week-1/ts-examples/person.interface.ts
+++ week-1/ts-examples/person.interface.ts
@@ -1,10 +1,10 @@
 /*
 ============================================
-; Title: Exercise 1.4 - TypeScript
+; Title: Assignment 1.4 - TypeScript
 ; Author: Professor Krasso
 ; Date: 24 May 2022
 ; Modified By: Joel Hartung
-; Description: Exercise 1.4 - TypeScript
+; Description: Assignment 1.4 - TypeScript
 ; Code Attribution: Additional code from WEB 425 Exercise 1.4 documentation & video
 ;===========================================
 */
