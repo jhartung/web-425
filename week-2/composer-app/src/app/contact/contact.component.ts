547328bbaae0d61767d7ebd865a0a6500cc6836e
jhartung <joel.hartung@gmail.com>
2022-06-05 13:42:15 -0500

    Assignment 2.4

--- week-2/composer-app/src/app/contact/contact.component.ts
+++ week-2/composer-app/src/app/contact/contact.component.ts
@@ -1,3 +1,14 @@
+/*
+============================================
+; Title: Assignment 2.4 - Routing in Action
+; Author: Professor Krasso
+; Date: 31 May 2022
+; Modified By: Joel Hartung
+; Description: Assignment 2.4 - Routing in Action
+; Code Attribution: Additional code from WEB 425 Week 2 exercises and videos
+;===========================================
+*/
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
