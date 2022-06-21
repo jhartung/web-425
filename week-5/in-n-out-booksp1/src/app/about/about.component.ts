bc8322af69ba73b118e89ac006e9e1198a6ff6ef
jhartung <joel.hartung@gmail.com>
2022-06-21 11:12:23 -0500

    Exercise 5.2

---
+++ week-5/in-n-out-booksp1/src/app/about/about.component.ts
@@ -0,0 +1,26 @@
+/*
+============================================
+; Title: Assignment 5.2 - Navigation and Layout
+; Author: Professor Krasso
+; Date: 21 June 2022
+; Modified By: Joel Hartung
+; Title: Assignment 5.2 - Navigation and Layout
+; Code Attribution: Additional code from WEB 425 Week 5 exercises and videos
+;===========================================
+*/
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
